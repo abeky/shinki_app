1159c0c769abff7e12b28db8d5901a6fb5dafa40
abeky <kazuma.abe01@gmail.com>
2018-04-25 18:57:17 +0900

    Initial Commit

---
+++ shinki_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shinki_app
+//
+//  Created by 阿部一真 on 2018/04/25.
+//  Copyright © 2018年 阿部一真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
